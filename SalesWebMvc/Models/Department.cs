7eb9bd6da52396babde254b5c1d67ec2956121f6
João Paulo Ferreira de Souza <joao.ferreiradesouza@hotmail.com>
2019-05-31 19:22:19 -0300

    Other entities and second migrations

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,9 +1,34 @@
-﻿namespace SalesWebMvc.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMvc.Models
 {
     public class Department
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        ICollection<Seller> Sellers { get; set; }
 
+        public Department()
+        {
+            Sellers = new List<Seller>();
+        }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSaller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime inital, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(inital, final));
+        }
     }
 }
